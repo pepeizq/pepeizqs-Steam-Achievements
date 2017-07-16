dd0038ce8cb3d873f1dd8a1ac38e61e621f6c17c
Jose Luis Izquierdo <pepeizq@msn.com>
2017-07-16 17:45:00 +0200

    Agregar archivos de proyecto.

---
+++ Steam Achievements/MainPage.xaml.vb
@@ -0,0 +1,134 @@
+﻿Imports Microsoft.Services.Store.Engagement
+Imports Microsoft.Toolkit.Uwp
+Imports Microsoft.Toolkit.Uwp.Helpers
+Imports Windows.ApplicationModel.Core
+Imports Windows.Storage
+Imports Windows.System
+Imports Windows.UI
+
+Public NotInheritable Class MainPage
+    Inherits Page
+
+    Private Async Sub Page_Loaded(sender As Object, e As RoutedEventArgs)
+
+        'Windows.Globalization.ApplicationLanguages.PrimaryLanguageOverride = "es-ES"
+        'Windows.Globalization.ApplicationLanguages.PrimaryLanguageOverride = "en-US"
+
+        Acrilico.Generar(gridTopAcrilico)
+        Acrilico.Generar(gridMenuAcrilico)
+
+        Dim barra As ApplicationViewTitleBar = ApplicationView.GetForCurrentView().TitleBar
+        barra.ButtonBackgroundColor = Colors.Transparent
+        barra.ButtonForegroundColor = Colors.White
+        barra.ButtonPressedBackgroundColor = Colors.DarkRed
+        barra.ButtonInactiveBackgroundColor = Colors.Transparent
+        Dim coreBarra As CoreApplicationViewTitleBar = CoreApplication.GetCurrentView.TitleBar
+        coreBarra.ExtendViewIntoTitleBar = True
+
+        '--------------------------------------------------------
+
+        Dim recursos As Resources.ResourceLoader = New Resources.ResourceLoader()
+
+        botonVotarTexto.Text = recursos.GetString("Boton Votar")
+        botonMasCosasTexto.Text = recursos.GetString("Boton Cosas")
+
+        botonMasAppsTexto.Text = recursos.GetString("Boton Web")
+        botonContactoTexto.Text = recursos.GetString("Boton Contacto")
+        botonReportarTexto.Text = recursos.GetString("Boton Reportar")
+        botonCodigoFuenteTexto.Text = recursos.GetString("Boton Codigo Fuente")
+
+        '----------------------------------------------
+
+
+
+    End Sub
+
+    Private Sub GridVisibilidad(grid As Grid, boton As Button)
+
+        tbTitulo.Text = "Steam Achievements (" + SystemInformation.ApplicationVersion.Major.ToString + "." + SystemInformation.ApplicationVersion.Minor.ToString + "." + SystemInformation.ApplicationVersion.Build.ToString + "." + SystemInformation.ApplicationVersion.Revision.ToString + ")"
+
+        gridCategorias.Visibility = Visibility.Collapsed
+        gridConfig.Visibility = Visibility.Collapsed
+
+        grid.Visibility = Visibility.Visible
+
+        botonCategorias.Background = New SolidColorBrush(Colors.Transparent)
+        botonConfig.Background = New SolidColorBrush(Colors.Transparent)
+
+        If Not boton Is Nothing Then
+            boton.Background = New SolidColorBrush(Colors.SteelBlue)
+        End If
+
+    End Sub
+
+    Private Sub BotonCategorias_Click(sender As Object, e As RoutedEventArgs) Handles botonCategorias.Click
+
+        Dim recursos As Resources.ResourceLoader = New Resources.ResourceLoader()
+        GridVisibilidad(gridCategorias, botonCategorias, recursos.GetString("Categorias"))
+        GridSeleccionVisibilidad(gridSeleccionUserscore, buttonSeleccionUserscore)
+
+    End Sub
+
+    Private Sub BotonConfig_Click(sender As Object, e As RoutedEventArgs) Handles botonConfig.Click
+
+        Dim recursos As Resources.ResourceLoader = New Resources.ResourceLoader()
+        GridVisibilidad(gridConfig, botonConfig, recursos.GetString("Boton Config"))
+        GridVisibilidadConfig(gridConfigCategorias, botonConfigCategorias)
+
+    End Sub
+
+    Private Async Sub BotonVotar_Click(sender As Object, e As RoutedEventArgs) Handles botonVotar.Click
+
+        Await Launcher.LaunchUriAsync(New Uri("ms-windows-store:REVIEW?PFN=" + Package.Current.Id.FamilyName))
+
+    End Sub
+
+    Private Sub BotonMasCosas_Click(sender As Object, e As RoutedEventArgs) Handles botonMasCosas.Click
+
+        If popupMasCosas.IsOpen = True Then
+            botonMasCosas.Background = New SolidColorBrush(Colors.Transparent)
+            popupMasCosas.IsOpen = False
+        Else
+            botonMasCosas.Background = New SolidColorBrush(Colors.SteelBlue)
+            popupMasCosas.IsOpen = True
+        End If
+
+    End Sub
+
+    Private Sub PopupMasCosas_LayoutUpdated(sender As Object, e As Object) Handles popupMasCosas.LayoutUpdated
+
+        popupMasCosas.Height = spMasCosas.ActualHeight
+
+    End Sub
+
+    Private Async Sub BotonMasApps_Click(sender As Object, e As RoutedEventArgs) Handles botonMasApps.Click
+
+        Await Launcher.LaunchUriAsync(New Uri("https://pepeizqapps.com/"))
+
+    End Sub
+
+    Private Async Sub BotonContacto_Click(sender As Object, e As RoutedEventArgs) Handles botonContacto.Click
+
+        Await Launcher.LaunchUriAsync(New Uri("https://pepeizqapps.com/contact/"))
+
+    End Sub
+
+    Private Async Sub BotonReportar_Click(sender As Object, e As RoutedEventArgs) Handles botonReportar.Click
+
+        If StoreServicesFeedbackLauncher.IsSupported = True Then
+            Dim ejecutador As StoreServicesFeedbackLauncher = StoreServicesFeedbackLauncher.GetDefault()
+            Await ejecutador.LaunchAsync()
+        Else
+            Await Launcher.LaunchUriAsync(New Uri("https://pepeizqapps.com/contact/"))
+        End If
+
+    End Sub
+
+    Private Async Sub BotonCodigoFuente_Click(sender As Object, e As RoutedEventArgs) Handles botonCodigoFuente.Click
+
+        Await Launcher.LaunchUriAsync(New Uri("https://github.com/pepeizq/Steam-Categories"))
+
+    End Sub
+
+
+End Class
